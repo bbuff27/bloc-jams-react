a289b1a1055f4a2914e7ca775b30207e6b16ac1d
Bill Buff <bbuff27@gmail.com>
2018-01-21 15:52:34 -0700

    Add nav links

--- src/App.js
+++ src/App.js
@@ -10,11 +10,15 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+          <nav>
+            <Link to = "/">Landing</Link>
+            <Link to = "/library">Library</Link>
+          </nav>
           <h1>Bloc Jams</h1>
         </header>
         <main>
           <Route exact path = "/" component = { Landing } />
-          <Route path = "/library" component { Library } />
+          <Route path = "/library" component = { Library } />
         </main>
       </div>
     );
