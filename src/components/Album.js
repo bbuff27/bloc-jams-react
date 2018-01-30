85e7851c49945c14cc21a6da34288bccf77bde8c
Bill Buff <bbuff27@gmail.com>
2018-01-29 19:01:02 -0700

    Add time slider functionality

--- src/components/Album.js
+++ src/components/Album.js
@@ -12,12 +12,33 @@ class Album extends Component {
         this.state = {
           album: album,
           currentSong: album.songs[0],
+          currentTime: 0,
+          duration: album.songs[0].duration,
           isPlaying: false
         };
 
         this.audioElement = document.createElement("audio");
         this.audioElement.src = album.songs[0].audioSrc;
       }
+
+  componentDidMount(){
+    this.eventListeners = {
+      timeupdate: e => {
+        this.setState({ currentTime: this.audioElement.currentTime });
+      },
+      durationchange: e => {
+        this.setState({ duration: this.audioElement.duration });
+      }
+    }
+    this.audioElement.addEventListener("timeupdate", this.eventListeners.timeupdate);
+    this.audioElement.addEventListener("durationchange", this.eventListeners.durationchange);
+  }
+
+  componentWillUnmount(){
+    this.audioElement.src = null;
+    this.audioElement.removeEventListener("timeupdate", this.eventListeners.timeupdate);
+    this.audioElement.removeEventListener("durationchange", this.eventListeners.durationchange);
+  }
 
   play(){
     this.audioElement.play();
@@ -36,12 +57,18 @@ class Album extends Component {
 
   handleSongClick(song){
     const isSameSong = this.state.currentSong === true;
-    if(this.state.isPlaying && isSameSong){
+    if(this.state.isPlaying && isSameSong) {
       this.pause();
-    }else{
+    } else {
       if(!isSameSong) { this.setSong(song); }
       this.play();
     }
+  }
+
+  handleTimeChange(e){
+    const newTime = this.audioElement.duration * e.target.value;
+    this.audioElement.currentTime = newTime;
+    this.setState({ currentTime: newTime });
   }
 
   handlePrevClick(song){
@@ -98,6 +125,9 @@ class Album extends Component {
       <PlayerBar
       isPlaying={ this.state.isPlaying }
       currentSong={ this.state.currentSong }
+      currentTime={ this.audioElement.currentTime }
+      duration={ this.audioElement.duration }
+      handleTimeChange={ (e) => this.handleTimeChange(e) }
       handleSongClick={ () => this.handleSongClick(this.state.currentSong) }
       handlePrevClick={ () => this.handlePrevClick() }
       handleNextClick={ () => this.handleNextClick() }
